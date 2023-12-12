8534b01f6245f9a3776cde2b53d1aeeddde1c68e
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-12-12 17:39:31 +0100

    Integrate apollo server for graphql

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,21 +1,51 @@
-import "reflect-metadata"
+import "reflect-metadata";
 import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
 import middleware from '../middleware/common'
 import { applyMiddleware } from "../middleware";
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import http from 'http';
 
+
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world',
+  },
+};
 
 const app = express();
+const httpServer = http.createServer(app);
 const port = parseInt(process.env.PORT || '8443', 10);
 
+// Set up Apollo Server
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+
 applyMiddleware(middleware, app);
-// app.use(router) // Add the routes
 
-app.get("/", (_, res) => {
-  res.send("Welcome to EvvelandAI Agent");
-});
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+const startServer = async () => {
+  await apolloServer.start()
+  app.use(expressMiddleware(apolloServer))
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`GraphQL playground at http://localhost:${port}/graphql`);
+  });
+}
+
+startServer()
